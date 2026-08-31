50d0264734f54dc2360d0d249d7154e49535213b
meghcodes <marellameghana99@gmail.com>
2026-08-30 21:32:29 -0500

    add css for tenant login

--- src/components/Pages/Login/TenantLogin/TenantLogin.jsx
+++ src/components/Pages/Login/TenantLogin/TenantLogin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "./TenantLogin.css"
 
 export default function TenantLogin() {
   const [email, setEmail] = useState("");
